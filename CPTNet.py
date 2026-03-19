5d20d1d2bbf0dd901a1e74b8d3230572eb683fa3
profCRB <90035314+profCRB@users.noreply.github.com>
2026-03-19 18:36:51 +0530

    Clarify file loading instructions in comments
    Updated comments for file loading instructions to clarify that users should uncomment and edit according to their file directory.

--- CPTNet.py
+++ CPTNet.py
@@ -609,13 +609,37 @@ Vld_IP=[0,10,71,41,11,24,36,10,23,5,48,122,29,10,63,19,10]
 
 
 
-## File loading coding lines, Uncomment accordingly
+## File loading coding lines, Uncomment and edit according to your file directory
 # file_path_temp = '/content/drive/MyDrive/IndianPines/indian_pines_corrected.npy'
 # temp = np.load(file_path_temp)
 # file_path_gt_temp = '/content/drive/MyDrive/IndianPines/indian_pines_gt.npy'
 # gt_temp=np.load(file_path_gt_temp)
 # temp_og=np.copy(temp)
 # temp=temp.reshape(temp.shape[0],temp.shape[1],20,10)
+# temp,pca_obj=PCA_fit_transform(temp,5)
+# temp=np.transpose(temp,axes=[0,1,3,2])
+# print(temp.shape)
+
+## File loading coding lines, Uncomment and edit according to your file directory
+# file_path_temp = '/content/drive/MyDrive/Salinas/salinas_corrected.npy'
+# temp = np.load(file_path_temp)
+# file_path_gt_temp = '/content/drive/MyDrive/Salinas/salinas_gt.npy'
+# gt_temp=np.load(file_path_gt_temp)
+# temp_og=np.copy(temp)
+# temp=temp[:,:,0:200]
+# temp=temp.reshape(temp.shape[0],temp.shape[1],20,10)
+# temp,pca_obj=PCA_fit_transform(temp,5)
+# temp=np.transpose(temp,axes=[0,1,3,2])
+# print(temp.shape)
+
+## File loading coding lines, Uncomment and edit according to your file directory
+# file_path_temp = '/content/drive/MyDrive/PaviaCentre/pavia_centre.npy'
+# temp = np.load(file_path_temp)
+# file_path_gt_temp = '/content/drive/MyDrive/PaviaCentre/pavia_centre_gt.npy'
+# gt_temp=np.load(file_path_gt_temp)
+# temp_og=np.copy(temp)
+# temp=temp[:,:,0:100]
+# temp=temp.reshape(temp.shape[0],temp.shape[1],10,10)
 # temp,pca_obj=PCA_fit_transform(temp,5)
 # temp=np.transpose(temp,axes=[0,1,3,2])
 # print(temp.shape)
